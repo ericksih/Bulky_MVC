0daccac84f14e872387dfaafa34cbd0a79560f46
Erick <erick.saputra08@gmail.com>
2025-02-28 23:34:39 +0700

    Init: Init new project using Razor

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Name")] // display name will be use at label on form create
+        [MaxLength(30)] // validation server side for max input 30 char
+        public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")] // validation server side to only accept range 1 to 100 and custom error msg
+        public int DisplayOrder { get; set; }
+    }
+}
