85921913c788789380984532e040b2865c14c072
Erick <erick.saputra08@gmail.com>
2025-03-01 00:38:03 +0700

    feat: feature delete and edit.

---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category); // what we doing is added category obj to categories table
+                _db.SaveChanges(); // then save it to db.
+
+                return RedirectToPage("Index"); // go to index is meaning excecute to render list category
+            }
+            return Page();
+        }
+    }
+}
