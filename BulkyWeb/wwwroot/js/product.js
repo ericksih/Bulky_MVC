1c841eee35c37c4108912516e23300279bfb5ec8
Erick <erick.saputra08@gmail.com>
2025-03-23 02:18:17 +0700

    feat: implement sweetalert for delete confirmation.

---
+++ BulkyWeb/wwwroot/js/product.js
@@ -0,0 +1,64 @@
+﻿
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            { data: 'title' },
+            { data: 'isbn' },
+            { data: 'listPrice' },
+            { data: 'author' },
+            { data: 'category.name' },
+            { data: 'description' },
+            {
+                data: 'id',
+                render: function (data) {
+                    return `
+                        <div class="btn-group">
+                            <a href="/admin/product/upsert?id=${data}" class="btn btn-sm btn-outline-primary mx-2">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                            <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-sm btn-outline-danger mx-2">
+                                <i class="bi bi-trash-fill"></i>
+                            </a>
+                        </div>
+                    `;
+                },
+                orderable: false
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
+
+
+
